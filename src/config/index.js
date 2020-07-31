70e6ed545b2920cb1eb313178a77582d565c34b3
Murilo Maldonado Mendes <murilomaldonadom@gmail.com>
2020-07-31 15:26:11 -0300

    fix: Heroku URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const URL_BACKEND = window.location.host.includes('localhost') ? 'http://localhost:8080' : 'https://reactflix-murilo3m.herokuapp.com/categorias';
+const URL_BACKEND = window.location.host.includes('localhost') ? 'http://localhost:8080' : 'https://reactflix-murilo3m.herokuapp.com';
 
 export default {
   URL_BACKEND,
